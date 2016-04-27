66f6a0e09e98ec0bac5cb424a52e8e4d72735847
Kaysser Emad Kayyali <kaysserk.admin@kkayyali.local>
2016-04-27 19:49:44 -0400

    Did a dumb move, fixed

--- multiples.rb
+++ multiples.rb
@@ -1,13 +1,20 @@
 class MultipleAdder
+
 	def sum_of_multiples(n)
 		counter = 0
 		sum = 0
-		while counter <= n do 
-			if counter % 3 || counter % 5 
-				sum = sum + counter
+		while counter < n do 
+			if counter % 3 == 0 || counter % 5 == 0 
+				sum += counter
 			end
 			counter = counter + 1
 		end
-		return counter
+		return sum
 	end
 end
+
+
+adder = MultipleAdder.new
+
+puts adder.sum_of_multiples(500)
+puts adder.sum_of_multiples(1000)
